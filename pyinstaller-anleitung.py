2f2dac7f5bfc086f00daf8640a2bb941e42899a7
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-05-12 13:24:43 +0200

    wip

---
+++ pyinstaller-anleitung.py
@@ -0,0 +1,4 @@
+# Executable erstellen mit pyinstaller
+# python -m pip install pyinstaller
+# Navigiere zu: ...\AppData\Local\Programs\Python\Python310\Scripts\pyinstaller.exe --onefile <FILENAME>.py 
+
